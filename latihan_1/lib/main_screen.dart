9c85508bfeb7318a19485ccf41d743a896675d36
dinoarla <dinoarla@gmail.com>
2022-05-25 09:15:53 +0700

    Update Part 3-4

--- latihan_1/lib/main_screen.dart
+++ latihan_1/lib/main_screen.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:latihan_1/detail_screen.dart';
 
 import 'data_model.dart';
 
@@ -13,10 +14,12 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  bool isLoading = false;
+  List<DataModel> items = [];
   int page = 1;
   bool hasMore = true;
-  List<DataModel> items = [];
+  bool isLoading = false;
+
+  final scrollController = ScrollController();
 
   Future fetch() async {
     if (isLoading) return;
@@ -48,35 +51,81 @@ class _MainScreenState extends State<MainScreen> {
     }
   }
 
+  Future refresh() async {
+    setState(() {
+      items.clear();
+      page = 1;
+      hasMore = true;
+      isLoading = false;
+    });
+
+    fetch();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    fetch();
+
+    scrollController.addListener(() {
+      if (scrollController.position.maxScrollExtent ==
+          scrollController.offset) {
+        fetch();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Data Pelanggan'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (() {
-          setState(() {
-            fetch();
-          });
-        }),
-        child: const Icon(Icons.add),
-      ),
       body: Container(
         margin: const EdgeInsets.symmetric(horizontal: 8),
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: ((context, index) {
-            return Card(
-              child: ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage:
-                        NetworkImage(items[index].imageLocationUrl),
+        child: RefreshIndicator(
+          onRefresh: refresh,
+          child: ListView.builder(
+            controller: scrollController,
+            itemCount: items.length + 1,
+            itemBuilder: ((context, index) {
+              if (index < items.length) {
+                final item = items[index];
+                return Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(items[index].imageLocationUrl),
+                    ),
+                    title: Text(items[index].name),
+                    subtitle: Text(items[index].phoneNumber),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) => DetailScreen(item: item)),
+                        ),
+                      );
+                    },
                   ),
-                  title: Text(items[index].name),
-                  subtitle: Text(items[index].phoneNumber)),
-            );
-          }),
+                );
+              }
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: hasMore
+                    ? const Center(child: CircularProgressIndicator())
+                    : const Center(child: Text('Tidak ada data lagi')),
+              );
+            }),
+          ),
         ),
       ),
     );
